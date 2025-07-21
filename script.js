32de11b943ff21aaa6a3ba755c01d72855113033
Neo3691102 <nestor8772@gmail.com>
2025-07-20 19:48:19 -0600

    Se agrega axios en el proyecto y eventos para el search bar y el boton

---
+++ script.js
@@ -0,0 +1,29 @@
+const searchinput = document.getElementById("searchbarinput"); //input del hero
+const searchbutton = document.getElementById("searchbutton"); //botón de búsqueda del hero
+
+const searchMoviesOrSeries = async () => {
+    const query = searchinput.value.toLowerCase().trim();
+    if(query === ""){
+        alert("Por favor, ingresa un término de búsqueda.");
+        return;
+    }
+    if(query){
+        try{
+            const response = axios.get(`https://api.tvmaze.com/search/shows?q=${query}`);
+            const data = await response.data;
+            console.log(data);
+        }catch(error){
+            console.error("No se encontró ninguna serie o película con ese nombre.", error);
+        }
+    }
+        
+}
+
+document.getElementById("searchbutton").addEventListener("click", searchMoviesOrSeries);
+document.getElementById("searchbarinput").addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        e.preventDefault(); // Evita el envío del formulario
+        searchMoviesOrSeries();
+    }
+});
+    
