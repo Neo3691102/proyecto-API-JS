6685cab7887dd5b0c85e1b030f0c5224c23eb9b4
Neo3691102 <nestor8772@gmail.com>
2025-07-21 10:39:11 -0600

    Se crea evento para poder crear card y cargar series por default

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//barra y boton de busqueda del hero
 const searchinput = document.getElementById("searchbarinput"); //input del hero
 const searchbutton = document.getElementById("searchbutton"); //botón de búsqueda del hero
 
@@ -26,3 +27,39 @@ document.getElementById("searchbarinput").addEventListener("keypress", function 
     }
 });
     
+//cargar series al abrir el sitio
+const crearCard = (series) => {
+    const seriesContainer = document.getElementById("seriescontainer");
+    const card = document.createElement("div");
+    card.classList.add("seriecard");
+    const cardTitle = document.createElement("h3");
+    cardTitle.textContent = series.name; 
+    const cardImage = document.createElement("img");
+    cardImage.src = series.image.medium; 
+    const cardUrl =document.createElement("a");
+    cardUrl.textContent = "Ver más"; 
+    cardUrl.href = series.url; 
+    cardUrl.target = "blank"; 
+     
+    
+    card.appendChild(cardImage);
+    card.appendChild(cardTitle);
+    card.appendChild(cardUrl);
+   
+    
+    
+    seriesContainer.appendChild(card);
+
+}
+
+const loadSeries = async () => {
+    const response = await axios.get("https://api.tvmaze.com/shows");
+    const series = response.data;
+    console.log(series);
+    series.forEach(serie => {
+        crearCard(serie); //crear una card por cada serie
+    });
+    //mandar a llamar crearCard con el array de series
+}
+
+document.addEventListener("DOMContentLoaded", loadSeries);
